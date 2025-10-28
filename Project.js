a4f8724ebf374cd6e1c6118f983e94bf6bfd341e
Don-Ferrida <donferida08@gmail.com>
2025-10-28 20:39:42 +0530

    Removed old Files

--- Project.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const projectSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  liveDemo: String,
-  githubLink: String,
-});
-export default mongoose.model("Project", projectSchema);
