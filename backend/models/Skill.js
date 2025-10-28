2673f32d1db2f91e8c213ad23cc67f84de6579d9
Don-Ferrida <donferida08@gmail.com>
2025-10-28 20:32:50 +0530

    remove old folder

--- backend/models/Skill.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const skillSchema = new mongoose.Schema({
-  category: String,
-  skills: [String],
-});
-export default mongoose.model("Skill", skillSchema);
--- backend/models/Testimonial.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const testimonialSchema = new mongoose.Schema({
-  name: String,
-  designation: String,
-  message: String,
-  image: String,
-});
-export default mongoose.model("Testimonial", testimonialSchema);
